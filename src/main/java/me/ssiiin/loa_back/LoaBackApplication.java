d5d05945314477f2b38292ed7eaf943913719677
ssIIIn <gomee312@naver.com>
2024-08-18 21:08:49 +0900

    Initial commit

---
+++ src/main/java/me/ssiiin/loa_back/LoaBackApplication.java
@@ -0,0 +1,13 @@
+package me.ssiiin.loa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ssiiin/loa_back/LoaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssiiin.loa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
